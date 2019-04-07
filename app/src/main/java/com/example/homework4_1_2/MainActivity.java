982d79f77151bbffc94db37f62f3632a9f0da389
Anna <panova2006_89@mail.ru>
2019-04-07 17:02:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework4_1_2/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.homework4_1_2;
+
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPref;
+    private static final String TEXT = "text";
+    List<Map<String, String>> values = new ArrayList<>();
+    SwipeRefreshLayout swipeLayout;
+    SimpleAdapter listContentAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView list = findViewById(R.id.list);
+
+        sharedPref = getSharedPreferences("text", MODE_PRIVATE);
+
+        if (!(sharedPref.contains(TEXT))) {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString(TEXT, getString(R.string.large_text));
+            editor.apply();
+
+        }
+
+        prepareContent();
+
+        listContentAdapter = createAdapter(values);
+
+        list.setAdapter(listContentAdapter);
+
+        AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                values.remove(position);
+                SimpleAdapter adapter = (SimpleAdapter) parent.getAdapter();
+                adapter.notifyDataSetChanged();
+            }
+        };
+
+        list.setOnItemClickListener(listener);
+
+        swipeLayout=findViewById(R.id.swiperefresh);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
+            @Override
+            public void onRefresh(){
+                prepareContent();
+                listContentAdapter.notifyDataSetChanged();
+
+                swipeLayout.setRefreshing(false);
+            }
+        });
+
+    }
+
+    @NonNull
+    private SimpleAdapter createAdapter(List<Map<String, String>> values) {
+        return new SimpleAdapter(this,
+                values,
+                R.layout.list_item,
+                new String[]{"name","text"},
+                new int[]{R.id.name,R.id.text});
+    }
+
+    private void prepareContent(){
+        values.clear();
+        String[] arrayContent = sharedPref.getString(TEXT, "").split("\n\n");
+        for (int i = 0; i < arrayContent.length; i++){
+            Map<String, String> value = new HashMap<>();
+            value.put("name", String.valueOf(arrayContent[i].length()));
+            value.put("text", arrayContent[i]);
+            values.add(value);
+        }
+    }
+
+
+}
